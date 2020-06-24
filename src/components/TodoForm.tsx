c734ffe37795ace9e48313de4bd978a2c31c4c1c
Daniil_Sapielkin <vipdanik98@gmail.com>
2020-06-24 09:42:38 +0300

    first comit

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,42 @@
+import React, {useState,useRef} from 'react'
+
+interface TodoFormProps  {
+ onAdd(title:string):void,
+}
+
+export const TodoForm:React.FC<TodoFormProps> =(props)=> {
+
+// const [title,setTitle] =useState<string>("")
+// const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setTitle(event.target.value)
+// }
+// const keyPressHandler = (event:React.KeyboardEvent)=>{
+//     if(event.key === "Enter") {
+        // console.log(title);
+       // setTitle('')
+//     }
+// }
+//useRef
+const ref = useRef<HTMLInputElement>(null)
+
+const keyPressHandler = (event:React.KeyboardEvent)=>{
+    if(event.key === "Enter") {
+        props.onAdd(ref.current!.value)
+        // console.log(ref.current!.value)   
+        ref.current!.value = ""
+    }
+}
+
+    return(
+        <div className ="input-field mt2">
+            <input
+            ref = {ref}
+            type="text" id ="titile" placeholder="Введите название дела"
+            onKeyPress = {keyPressHandler}    
+            //  value ={title} 
+            // onChange = {changeHandler}
+            />
+            <label htmlFor="title" className="active">Введите название дела</label>
+        </div>
+    )
+}
